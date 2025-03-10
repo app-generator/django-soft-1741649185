039d3ab4375f2d1673bb1e284c51c7aa0165ce1e
App Generator <51070104+app-generator@users.noreply.github.com>
2025-03-11 01:26:34 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Student(models.Model):
+
+    #__Student_FIELDS__
+    id = models.IntegerField(null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    age_group = models.IntegerField(null=True, blank=True)
+
+    #__Student_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Student")
+        verbose_name_plural = _("Student")
+
+
+
+#__MODELS__END
